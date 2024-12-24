a335745c39d783bfdb3864809104dac7e4402e42
EdilsonBaggio <edilsonbaggio_@hotmail.com>
2024-12-24 13:28:22 -0300

    Feat Alfatur: Cadastro de usuarios e edição

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,39 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create([
             'name' => 'Edilson',
             'email' => 'developer@symbster.com',
-            'password' => 'mudar123!'
+            'password' => Hash::make('mudar123!'), // Criptografando a senha
+            'role' => 'Administrador',
+            'rut' => '12345678901',
+            'whatsapp' => '(99) 99999-9999',
+            'commission_percentage' => 10.5,
+        ]);
+    }
+}
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Edilson',
+            'email' => 'developer@symbster.com',
+            'password' => Hash::make('mudar123!'), // Criptografando a senha
+            'role' => 'Administrador',
+            'rut' => '12345678901',
+            'whatsapp' => '(99) 99999-9999',
+            'commission_percentage' => 10.5,
         ]);
     }
 }
