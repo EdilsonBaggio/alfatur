0f12b35cbc9be39f588ac74a0072afc2157223c9
EdilsonBaggio <edilsonbaggio_@hotmail.com>
2025-01-01 21:31:21 -0300

    Feat Vendas criação do cadastro de vendas por usuário e relação com tabela de tours

--- resources/views/tours/create.blade.php
+++ resources/views/tours/create.blade.php
@@ -9,30 +9,31 @@
             <form method="POST" action="{{ route('tours.store') }}">
                 @csrf
                 <div class="mb-3">
-                    <label class="form-label">Nome Tour:</label>
+                    <label class="form-label">Nombre Tour:</label>
                     <input type="text" name="name" class="form-control" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Preço Venda:</label>
+                    <label class="form-label">Precio Venta:</label>
                     <input type="number" name="price" class="form-control" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Preço Venda Mínimo:</label>
+                    <label class="form-label">Precio Venta Mínimo:</label>
                     <input type="number" name="min_price" class="form-control" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Custo:</label>
+                    <label class="form-label">Costo:</label>
                     <input type="number" name="cost" class="form-control" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Preço Infantil:</label>
+                    <label class="form-label">Precio Venta Infante:</label>
                     <input type="number" name="child_price" class="form-control" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Custo Infantil:</label>
+                    <label class="form-label">Costo Infante:</label>
                     <input type="number" name="child_cost" class="form-control" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Criar Tour</button>
+                <span>*Dato requerido, si no tiene valor ingrese 0 (ciero).</span><br>
+                <button type="submit" class="btn btn-primary mt-3">Criar Tour</button>
             </form>
         </div>
     </div>
