003b62fd1c604b3cfd77067bec8aed125936be1f
EdilsonBaggio <edilsonbaggio_@hotmail.com>
2025-01-03 16:43:15 -0300

    Feat Estimativo: Tela para estimativo de vendas

--- app/Http/Controllers/EstimativoController.php
+++ app/Http/Controllers/EstimativoController.php
@@ -3,30 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Venda;
 use App\Models\Tour;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class EstimativoController extends Controller
 {
     public function index(Request $request)
     {
         // Obtém a data inicial passada pelo usuário ou usa a data atual
-        $startDate = $request->input('filterDate', Carbon::now()->format('Y-m-d'));  // Data do filtro ou data atual
+        $startDate = $request->input('filterDate', Carbon::now()->format('Y-m-d'));  
         
         // Garante que a data seja no formato correto e sem horário
-        $startDate = Carbon::parse($startDate)->startOfDay();  // Início do dia
-        $endDate = $startDate->copy()->addDays(90)->endOfDay(); // Final do dia após 14 dias
+        $startDate = Carbon::parse($startDate)->startOfDay();  
+        $endDate = $startDate->copy()->addDays(14)->endOfDay(); 
 
-        // Busca os tours com dados vinculados à venda dentro do intervalo de 14 dias
+        // Busca os tours com dados vinculados à venda e filtra pelo user_id
         $tours = Tour::with('venda')
-            ->whereBetween('data_tour', [$startDate, $endDate])  // Utiliza whereBetween para filtrar entre o intervalo
+            ->whereBetween('data_tour', [$startDate, $endDate]) // Intervalo de 14 dias
+            ->whereHas('venda', function ($query) { // Filtro pela tabela vendas
+                $query->where('user_id', Auth::id());
+            })
             ->orderBy('data_tour')
             ->get();
 
-        // Agrupa os tours por data (formato alterado para 'Y-m-d' para consistência)
+        // Agrupa os tours por data
         $groupedTours = $tours->groupBy(function ($tour) {
-            return Carbon::parse($tour->data_tour)->format('Y-m-d'); // Agrupa por data no formato 'Y-m-d'
+            return Carbon::parse($tour->data_tour)->format('Y-m-d'); 
         });
 
         // Retorna a view com os dados e a data de filtro
