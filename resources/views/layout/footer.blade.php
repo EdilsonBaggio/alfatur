b214334290087a9dea6cc5d32d1d42f03ff982a8
EdilsonBaggio <edilsonbaggio_@hotmail.com>
2025-11-29 15:42:03 -0300

    feat Site: Ajustando novo modelo para venda

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="container content-rodape">
-        <p>ALFATUR Chile - Todos los Derechos Reservados</p>
+        <p>Turismo Chile - Todos los Derechos Reservados</p>
     </div>
 </footer>
